4000e6f9e555e1658b448247955b819d4c72cedc
Nathan Burns <burnzoire@gmail.com>
2018-05-16 09:35:54 +1000

    adds correct datatypes to model

--- app/models/gaia_source/import.rb
+++ app/models/gaia_source/import.rb
@@ -4,23 +4,28 @@ class GaiaSource
   class Import
     def gaia_source_csv(csv)
       gaia_sources = []
+      puts 'Counting records...'
       size = CSV.read(csv, headers: true).count
       puts "Begin processing #{size} records..."
       CSV.foreach(csv, headers: true) do |row|
-        gaia_source = create_gaia_source_from_row(row)
-        gaia_sources << gaia_source if gaia_source.valid?
-        puts "processed #{$INPUT_LINE_NUMBER - 1} of #{size}"
+        gaia_source = create_from_row(row)
+        if gaia_source.valid?
+          gaia_sources << gaia_source
+          percent = ($INPUT_LINE_NUMBER - 1) / size.to_f * 100
+          print "\r"
+          print "processing #{percent.round(2).to_s.ljust(4, '0')}%     "
+        end
       end
-      puts 'Done!'
+      print "\r"
+      print 'Done!                                 '
       gaia_sources
     end
 
     private
 
-    def create_gaia_source_from_row(row)
+    def create_from_row(row)
       data = row.to_h.symbolize_keys
       gaia = GaiaSource.create!(data)
-      puts gaia.valid?
       gaia
     end
   end
